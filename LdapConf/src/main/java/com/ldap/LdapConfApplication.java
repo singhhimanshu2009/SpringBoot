2ada7050bae796b4fb05111c8a55f06464e69f53
singhhimanshu2009 <singhhimanshu2009@gmail.com>
2021-06-13 17:06:09 +0530

    LdapConfiguration

---
+++ LdapConf/src/main/java/com/ldap/LdapConfApplication.java
@@ -0,0 +1,13 @@
+package com.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapConfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapConfApplication.class, args);
+	}
+
+}
